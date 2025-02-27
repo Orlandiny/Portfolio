553713da2ceef2bf6f7cf759215a428479dcd1ee
Orlandini <orlando.quero77@gmail.com>
2025-02-27 14:17:59 -0400

    actualizacion

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    darkmode: 'class',
-    theme: {
-        extend: {},
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class', // Corregí "darkmode" a "darkMode" (es case-sensitive)
+  theme: {
+    extend: {
+      spacing: {
+        '22': '5.5rem', // 22 * 0.25rem = 5.5rem
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
